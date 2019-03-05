e4aad96b99be489fea6002e1d5d45b084c3daba3
Felipe <felipewww@outlook.com.br>
2019-03-05 09:27:08 -0300

    add docker files, initial setup

---
+++ _Dockerfiles/angular-admin/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm install
+ng serve --host 0.0.0.0
+
+#Keep container alive after end this script
+/bin/bash
---
+++ _Dockerfiles/endpoints-main/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm install
+npm install nodemon -g
+npm run dev
+
+#Keep container alive after end this script
+/bin/bash
